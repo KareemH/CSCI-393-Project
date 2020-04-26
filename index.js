13240a40293072042d136ac758c5497510daa3ea
Kareem Hussein <kareem@kareems-mbp.lan>
2020-04-25 20:02:34 -0400

    Create homepage

---
+++ index.js
@@ -0,0 +1,5 @@
+var receiveInput = function(){
+  var g_enzyme_set = $("#G-enzyme-set").val();
+  var uc_enzyme_set = $("#UC-enzyme-set").val();
+  console.log(g_enzyme_set + " " + uc_enzyme_set);
+};
